ea2cdd8c1514e8766fee885405717532284ddccb
Keelan <keelanmatthews123@gmail.com>
2022-10-09 10:10:47 +0200

    added scroll cta animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -45,71 +45,69 @@ export default function Hero() {
     ];
 
     return (
-        <div className='vh-100'>
-            <div className='pt-5'>
-                <Row className='pt-5 h-100'>
-                    <Col xs={12} md={1} className='d-flex flex-column justify-content-between align-items-center'>
-                        <FontAwesomeIcon icon={faEnvelope} className='fs-1' />
-                        <Navigation />
-                        <div></div>
-                    </Col>
-                    <Col xs={11} md={10}>
-                        <Row ref={parRef} className='position-relative'>
-                            <Col xs={12} md={6} className='p-5'>
-                                <p className={`m-0 japanese ${showHero ? 'visible' : ''}`}>デザイン</p>
-                                <div className="text-outline">
-                                    <TextReveal text="Keelan" visible={showHero} />
-                                    <TextReveal text="Matthews" visible={showHero} />
+        <div className='vh-100 pt-5'>
+            <Row className='pt-5 h-100'>
+                <Col xs={12} md={1} className='d-flex flex-column justify-content-between align-items-center'>
+                    <FontAwesomeIcon icon={faEnvelope} className='fs-1' />
+                    <Navigation />
+                    <div></div>
+                </Col>
+                <Col xs={11} md={10}>
+                    <Row ref={parRef} className='position-relative'>
+                        <Col xs={12} md={6} className='p-5'>
+                            <p className={`m-0 japanese ${showHero ? 'visible' : ''}`}>デザイン</p>
+                            <div className="text-outline">
+                                <TextReveal text="Keelan" visible={showHero} />
+                                <TextReveal text="Matthews" visible={showHero} />
+                            </div>
+                            <p className={`fs-2 slogan ${showHero ? 'visible' : ''}`}>
+                                <TextTransition springConfig={presets.gentle} inline>
+                                    {TEXTS[index % TEXTS.length]}
+                                </TextTransition>
+                                &nbsp;de
+                                <TextTransition springConfig={presets.gentle} inline>
+                                    {TEXTS2[index % TEXTS2.length]}
+                                </TextTransition>
+                                er.
+                            </p>
+                            <Button variant={`outline-secondary button ${showHero ? 'visible' : ''}`} size="lg" className='mt-5'>download cv</Button>
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <div className="position-relative h-100 w-100">
+                                <div className={`position-absolute painting ${showHero ? 'visible' : ''}`}>
+                                    <MouseParallax isAbsolutelyPositioned shouldResetPosition strength={0.01} parallaxContainerRef={parRef}>
+                                        <div style={{ width: '700px', height: '800px' }}>
+                                            <WaveImage />
+                                        </div>
+                                    </MouseParallax>
                                 </div>
-                                <p className={`fs-2 slogan ${showHero ? 'visible' : ''}`}>
-                                    <TextTransition springConfig={presets.gentle} inline>
-                                        {TEXTS[index % TEXTS.length]}
-                                    </TextTransition>
-                                    &nbsp;de
-                                    <TextTransition springConfig={presets.gentle} inline>
-                                        {TEXTS2[index % TEXTS2.length]}
-                                    </TextTransition>
-                                    er.
-                                </p>
-                                <Button variant={`outline-secondary button ${showHero ? 'visible' : ''}`} size="lg" className='mt-5'>download cv</Button>
-                            </Col>
-                            <Col xs={12} md={6}>
-                                <div className="position-relative h-100 w-100">
-                                    <div className={`position-absolute painting ${showHero ? 'visible' : ''}`}>
-                                        <MouseParallax isAbsolutelyPositioned shouldResetPosition strength={0.01} parallaxContainerRef={parRef}>
-                                            <div style={{ width: '700px', height: '800px' }}>
-                                                <WaveImage />
-                                            </div>
-                                        </MouseParallax>
-                                    </div>
-                                    <div className={`position-absolute circle ${showHero ? 'visible' : ''}`}>
-                                        <MouseParallax isAbsolutelyPositioned shouldResetPosition strength={0.03} parallaxContainerRef={parRef}>
-                                            <div className='bg-primary rounded-circle' style={{ width: '150px', height: '150px' }}></div>
-                                        </MouseParallax>
-                                    </div>
-                                    <div className={`bg-primary rounded-circle click-circle ${hideCircle ? 'clicked' : ''} ${showHero ? 'd-none' : ''}`} onClick={() => revealHero()} style={{ width: '150px', height: '150px' }}></div>
-                                    <div className={`position-absolute statue ${showHero ? 'visible' : ''}`}>
-                                        <MouseParallax isAbsolutelyPositioned shouldResetPosition strength={0.05} parallaxContainerRef={parRef}>
-                                            <div>
-                                                <img src="/images/statue.png" alt="statue" width={400} />
-                                            </div>
-                                        </MouseParallax>
-                                    </div>
+                                <div className={`position-absolute circle ${showHero ? 'visible' : ''}`}>
+                                    <MouseParallax isAbsolutelyPositioned shouldResetPosition strength={0.03} parallaxContainerRef={parRef}>
+                                        <div className='bg-primary rounded-circle' style={{ width: '150px', height: '150px' }}></div>
+                                    </MouseParallax>
                                 </div>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col xs={1} className="d-flex flex-column justify-content-between align-items-center">
-                        <div className="page-number">
-                            <p className='fs-1'>00</p>
-                        </div>
-                        <div className='scroll-down d-flex flex-column justify-content-center align-items-center'>
-                            <p className='fs-5'>scroll down</p>
-                            <div className="mt-5 scroll-line"></div>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
+                                <div className={`bg-primary rounded-circle click-circle ${hideCircle ? 'clicked' : ''} ${showHero ? 'd-none' : ''}`} onClick={() => revealHero()} style={{ width: '150px', height: '150px' }}></div>
+                                <div className={`position-absolute statue ${showHero ? 'visible' : ''}`}>
+                                    <MouseParallax isAbsolutelyPositioned shouldResetPosition strength={0.05} parallaxContainerRef={parRef}>
+                                        <div>
+                                            <img src="/images/statue.png" alt="statue" width={400} />
+                                        </div>
+                                    </MouseParallax>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col xs={1} className="d-flex flex-column justify-content-between align-items-center h-100">
+                    <div className="page-number">
+                        <p className='fs-1'>00</p>
+                    </div>
+                    <div className='scroll-down d-flex flex-column justify-content-center align-items-center'>
+                        <p className={`fs-5 scroll-down-text ${showHero ? 'visible' : ''}`}>scroll down</p>
+                        <div className={`mt-5 scroll-line ${showHero ? 'visible' : ''}`}></div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
