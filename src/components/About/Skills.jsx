4709c23d2a37418112385b804dee2284e766daee
Keelan <keelanmatthews123@gmail.com>
2022-10-24 11:17:33 +0200

    Implemented skill section

--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -1,9 +1,120 @@
 import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Accordion from 'react-bootstrap/Accordion'
+import { SiReact, SiBootstrap, SiJavascript, SiVuedotjs, SiPhp, SiSass } from 'react-icons/si'
+import { DiMysql } from 'react-icons/di'
+
+const SkillGroup = (skills) => {
+	return (
+		<>
+			{skills.map((skill, index) => (
+				<div className="border-black d-flex m-3 p-3 align-items-center" key={index} style={{ flexBasis: '20%' }}>
+					{skill.icon}
+					<div className="ms-4">
+						<p className="fw-bold m-0 p-0">{skill.name}</p>
+						<p className="fs-6 m-0 p-0">{skill.level}</p>
+					</div>
+				</div>
+			))}
+		</>
+	)
+}
 
 export default function Skills() {
-  return (
-    <div>
-        <h2 className='fw-bold mb-4'>My Skill Set</h2>
-    </div>
-  )
+	const skills = [
+		{
+			name: 'React',
+			level: 'Expert',
+			icon: <SiReact size={37} />,
+			top: true,
+			type: 'frontend'
+		},
+		{
+			name: 'Bootstrap',
+			level: 'Expert',
+			icon: <SiBootstrap size={37} />,
+			top: true,
+			type: 'frontend'
+		},
+		{
+			name: 'Javascript',
+			level: 'Expert',
+			icon: <SiJavascript size={37} />,
+			top: true,
+			type: 'frontend'
+		},
+		{
+			name: 'Vue',
+			level: 'Intermediate',
+			icon: <SiVuedotjs size={37} />,
+			top: true,
+			type: 'frontend'
+		},
+		{
+			name: 'PHP',
+			level: 'Intermediate',
+			icon: <SiPhp size={37} />,
+			top: true,
+			type: 'backend'
+		},
+		{
+			name: 'SASS',
+			level: 'Expert',
+			icon: <SiSass size={37} />,
+			top: true,
+			type: 'frontend'
+		},
+		{
+			name: 'MySQL',
+			level: 'Intermediate',
+			icon: <DiMysql size={37} />,
+			top: true,
+			type: 'backend'
+		}
+	]
+
+	return (
+		<Row className="mb-5 pb-5">
+			<Col xs={12} md={{ span: 11, offset: 1 }} className="d-flex justify-content-center flex-wrap mb-5">
+				{SkillGroup(skills.filter((skill) => skill.top))}
+			</Col>
+			<Col xs={12} md={{ span: 11, offset: 1 }} className="px-3 mt-5">
+				<Accordion>
+					<Accordion.Item eventKey="0">
+						<Accordion.Header>Frontend</Accordion.Header>
+						<Accordion.Body>
+							<div className="d-flex">
+								{SkillGroup(skills.filter((skill) => skill.type === 'frontend'))}
+							</div>
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="1">
+						<Accordion.Header>Backend</Accordion.Header>
+						<Accordion.Body>
+							<div className="d-flex">
+								{SkillGroup(skills.filter((skill) => skill.type === 'backend'))}
+							</div>
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="2">
+						<Accordion.Header>Software</Accordion.Header>
+						<Accordion.Body>
+							<div className="d-flex">
+								{SkillGroup(skills.filter((skill) => skill.type === 'software'))}
+							</div>
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="3">
+						<Accordion.Header>Programming</Accordion.Header>
+						<Accordion.Body>
+							<div className="d-flex">
+								{SkillGroup(skills.filter((skill) => skill.type === 'programming'))}
+							</div>
+						</Accordion.Body>
+					</Accordion.Item>
+				</Accordion>
+			</Col>
+		</Row>
+	)
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -15,22 +15,29 @@ export default function AboutPage() {
 				</Col>
 				<Col xs={10} className="scrollable">
 					<AboutTransition />
-					<Row className="d-flex align-items-center py-5">
-						<Col xs={12} md={6} className="d-flex justify-content-end">
-							<div className="w-75">
-								<h2 className='fw-bold mb-4'>Who I am</h2>
-								<p className="fs-5 border-start border-3 ps-3">
-									I am an aspiring Full Stack Developer from <span className="fw-bold">Johannesburg, South Africa</span>. Growing up, I have always had an insatiable desire for creativity and perfection. This, coupled up with my thorough enjoyment in coding, lead me down my current path.
-									I have a thorough passion for coding, be it in programming or markup languages. The idea of being able to produce anything from scratch intrigues me. This also includes designing vector graphics, 3D renders, animations and even music.<br /><br />
-									I am able to pick up new skills very quickly, and always complete my deadlines early. In the event of time constraints, I <span className="fw-bold">thrive</span> under pressure.
-								</p>
-							</div>
-						</Col>
-						<Col xs={12} md={6} className="text-center">
-							<img src="images/portrait.webp" alt="" />
-						</Col>
-					</Row>
-					<Skills />
+					<div className="vh-100 d-flex flex-column justify-content-center">
+						<h1 className="fw-bold heading-line mb-5">Introduction</h1>
+						<Row className="d-flex align-items-center">
+							<Col xs={12} md={6} className="d-flex justify-content-end">
+								<div className="w-75">
+									<p className='fw-bold mb-4 fs-3'>Who I am</p>
+									<p className="fs-5 border-start border-2 border-primary ps-3">
+										I am an aspiring Full Stack Developer from <span className="fw-bold">Johannesburg, South Africa</span>. Growing up, I have always had an insatiable desire for creativity and perfection. This, coupled up with my thorough enjoyment in coding, lead me down my current path.
+										I have a thorough passion for coding, be it in programming or markup languages. The idea of being able to produce anything from scratch intrigues me. This also includes designing vector graphics, 3D renders, animations and even music.<br /><br />
+										I am able to pick up new skills very quickly, and always complete my deadlines early. In the event of time constraints, I <span className="fw-bold">thrive</span> under pressure.
+									</p>
+								</div>
+							</Col>
+							<Col xs={12} md={6} className="text-center">
+								<img src="images/portrait.webp" alt="" />
+							</Col>
+						</Row>
+					</div>
+
+					<div className="vh-100 d-flex flex-column justify-content-center">
+						<h1 className="fw-bold heading-line mb-5">Skill Set</h1>
+						<Skills />
+					</div>
 				</Col>
 				<Col xs={2} md={1}>
 					<Row className="d-flex flex-column justify-content-between align-items-center h-100">
