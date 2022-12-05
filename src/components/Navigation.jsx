5f169e91999337038f589b945028f47f93b561ce
Keelan <keelanmatthews123@gmail.com>
2022-12-05 14:47:17 +0200

    Animated navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,13 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import { motion } from 'framer-motion/dist/framer-motion'
 
+const transition = { duration: 0.7, ease: [0.43, 0.13, 0.23, 0.96] }
+
+const navVariants = {
+    hidden: { opacity: 0, x: -10 },
+    visible: { opacity: 1, x: 0 }
+}
+
 export default function Navigation({ activeSection }) {
     return (
         <Navbar expand="md" className="navigation">
@@ -11,25 +18,30 @@ export default function Navigation({ activeSection }) {
                 <Navbar.Toggle aria-controls="nav-collapse" className="p-0" />
                 <Navbar.Collapse id="nav-collapse" className="d-flex justify-content-center">
                     <Nav className='d-flex flex-column'>
-                        <Nav.Link href="/#hero" className='p-0 nav-group'>
-                            <p className='m-0 nav-item'>hero</p>
-                            <div className={`nav-line ${activeSection === 'hero' ? 'active' : ''}`}></div>
-                        </Nav.Link>
-                        <Nav.Link href="/#about" className='p-0 nav-group'>
-                            <p className='m-0 nav-item'>about</p>
-                            <div className={`nav-line ${activeSection === 'about' ? 'active' : ''}`}></div>
-                        </Nav.Link>
-                        <Nav.Link href="/#projects" className='p-0 nav-group'>
-                            <p className='m-0 nav-item'>projects</p>
-                            <div className={`nav-line ${activeSection === 'projects' ? 'active' : ''}`}></div>
-                        </Nav.Link>
-                        <Nav.Link href="/#contact" className='p-0 nav-group'>
-                            <p className='m-0 nav-item'>contact</p>
-                            <div className={`nav-line ${activeSection === 'contact' ? 'active' : ''}`}></div>
-                        </Nav.Link>
+                        <NavLine section='hero' activeSection={activeSection} />
+                        <NavLine section='about' activeSection={activeSection} />
+                        <NavLine section='projects' activeSection={activeSection} />
+                        <NavLine section='contact' activeSection={activeSection} />
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+    )
+}
+
+const NavLine = ({ section, activeSection }) => {
+    return (
+        <Nav.Link href={`/#${section}`} className='p-0 nav-group'>
+            <motion.p
+                variants={navVariants}
+                initial="hidden"
+                whileHover="visible"
+                transition={transition}
+                className='m-0 nav-item'
+            >
+                {section}
+            </motion.p>
+            <div className={`nav-line ${activeSection === section ? 'active' : ''}`}></div>
+        </Nav.Link>
     )
 }
