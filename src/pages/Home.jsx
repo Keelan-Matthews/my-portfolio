6344a0e4701797cbc0c767f22293fa6338afb18a
Keelan <keelanmatthews123@gmail.com>
2022-12-19 15:08:33 +0200

    made site mobile message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Hero from '../components/Home/Hero';
 import Contact from '../components/Home/Contact';
 import SideColumns from '../components/SideColumns';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Layout from '../components/Layout';
 import ReactFullPage from '@fullpage/react-fullpage';
 import Section from '../components/Section';
 import Footer from '../components/Footer';
+import { motion } from 'framer-motion/dist/framer-motion';
 
 const anchors = ['hero', 'about', 'projects', 'contact', 'footer'];
 
@@ -15,9 +17,35 @@ export default function Home({ sections }) {
     const [hideScroll, setHideScroll] = useState(true);
     const [activeSection, setActiveSection] = useState('hero');
     const [page, setPage] = useState(0);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth < 768) {
+            setIsMobile(true);
+        }
+    }, [])
 
     return (
         <Layout title="Keelan Matthews | Welcome">
+            {
+                isMobile ? (
+                    <div className="mobile-message">
+                        <motion.div
+                            initial={{ opacity: 0, y: 100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }}
+                        >
+                            <h1>Sorry, this site is not mobile friendly yet.</h1>
+                            <p>Please view on a desktop or laptop.</p>
+                            <div className="d-flex">
+                                <Button variant="primary" className='mt-2' href="https://google.com">close</Button>
+                                <Button variant="outline-secondary" className='mt-2 ms-2' href="/keelan-matthews-cv.pdf" target="_blank">download cv</Button>
+                            </div>
+                        </motion.div>
+                    </div>
+                )
+                    : null
+            }
             <SideColumns scrollY={hideScroll} activeSection={activeSection} page={page}>
                 <Col xs={12} sm={10}>
                     <ReactFullPage
@@ -38,7 +66,7 @@ export default function Home({ sections }) {
                             setPage(destination.index);
                             setHideScroll(destination.index === 0);
                         }}
-                        
+
                         render={({ state, fullpageApi }) => {
 
                             return (
