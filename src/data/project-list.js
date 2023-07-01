5db49848e8fad548d507db935b31d9e53d4224dd
Keelan <keelanmatthews123@gmail.com>
2023-07-01 16:45:39 +0200

    Made site a bit more responsive

--- src/data/project-list.js
+++ src/data/project-list.js
@@ -12,7 +12,7 @@ export const projects = [
     {
         japanese: '輸送',
         title: 'Transport App',
-        site: 'https://www.figma.com/file/9Y99RlDKUofW0FyCKtddFB/Hi-Fi-Mockup-(Copy)?type=design&node-id=0-1&mode=design&t=ERWrvsQdtpDRCmgA-0'
+        site: 'https://www.figma.com/proto/9Y99RlDKUofW0FyCKtddFB/Hi-Fi-Mockup-(Copy)?page-id=0%3A1&type=design&node-id=1-3&viewport=479%2C198%2C0.14&t=uTcJ7ObA0lI9rjQL-1&scaling=scale-down&starting-point-node-id=1%3A3&mode=design'
     },
     {
         japanese: '奨学金',
